e2d5b4fb57d9a53a2618e93bc2aacc743b0c08d2
19312040-uti <alchris_alfara_pratista@teknokrat.ac.id>
2023-10-18 19:25:29 +0700

    renamed main to dasar_flutter, main is now a statefulWidget

---
+++ lib/dasar_flutter.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan 1',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan 1', style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.cyan,
+        ),
+        body: Column(
+          children: [
+            for (var i = 0; i < 6; i++)
+            Text("Pagi Dunia!"),
+            Row(
+              children: [
+                for (var i = 0; i < 6; i++)
+                Text('pagi magna! ')
+              ],
+            ),
+            Container(
+              color: Colors.blue,
+              height: 50,
+              width: 500,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: 
+                [for (var i = 0; i < 5; i++) Container(
+                color: Colors.red,
+                height: 30,
+                width: 30,
+                margin: EdgeInsets.all(10),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [for (var i = 0; i < 5; i++)
+              Container(
+                height: 30,
+                width: 30,
+                margin: EdgeInsets.all(10),
+                decoration:
+                  new BoxDecoration(
+                    color: Colors.cyan,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
